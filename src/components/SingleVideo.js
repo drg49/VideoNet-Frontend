134f4ca01288574a670f4eb792509d3082bee00a
Danny Gavin <homevpn14@gmail.com>
2021-06-25 13:53:25 -0700

    New Commit

--- src/components/SingleVideo.js
+++ src/components/SingleVideo.js
@@ -1,13 +1,19 @@
+import Nav from './Nav'
+
 const SingleVideo = (props) => {
 
     return (
+        <>
+        <Nav />
         <div id="single-vid-card">
             <video width="1200" id="single-vid" controls>
                 <source src={`https://drg-s3-3.s3.amazonaws.com/${props.video}`} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <h2>{props.title}</h2>
+            <h3>{props.username}</h3>
         </div>
+        </>
     )
 }
 
--- src/components/navcomp/Logo.js
+++ src/components/navcomp/Logo.js
@@ -1,13 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBolt } from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom'
 
 const icon = <FontAwesomeIcon icon={faBolt} />
 
 const Logo = () => {
     return (
+        <Link to="/">
         <div>
             <h1 id="logo">VideoNet {icon}</h1>
         </div>
+        </Link>
     )
 }
 
